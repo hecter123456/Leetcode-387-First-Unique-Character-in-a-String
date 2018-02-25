4dc749ababe03e587655d1dacda490615d22794c
hecter <hecter654321@gmail.com>
2018-02-25 14:56:36 +0800

    pass testcase: testSample by using itemgetter

--- First Unique Character in a String.py
+++ First Unique Character in a String.py
@@ -1,15 +1,30 @@
 import unittest
+import operator
 
 class unitest(unittest.TestCase):
     def testNone(self):
         Input = ""
         Output = -1
         self.assertEqual(Solution().firstUniqChar(Input),Output)
+    def testSample(self):
+        Input = "loveleetcode"
+        Output = 2
+        self.assertEqual(Solution().firstUniqChar(Input),Output)
 
 class Solution():
     def firstUniqChar(self, s):
-        if s == "":
-            return -1
+        dic = {}
+        for i in range(len(s)):
+            char = s[i]
+            if char in dic:
+                dic[char] = -1
+            else:
+                dic[char] = i
+        dic = sorted(dic.items(), key=operator.itemgetter(1))
+        for key in dic:
+            if key[1] != -1:
+                return key[1]
+        return -1
 
 if __name__ == '__main__':
     unittest.main()
